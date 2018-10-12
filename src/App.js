e65f0cfc1d7b98fa2dae750c999c8d530c384033
Aman Gulati <amangulati@Amans-MacBook-Pro.local>
2018-10-12 14:54:01 +0530

    demo works fine

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Tree from "rc-tree";
+import "rc-tree/assets/index.css";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.globalTreeData = [];
+  }
   render() {
+    this.props.hierarchyTree.forEach(x => this.rc(this.globalTreeData, x));
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <Tree defaultExpandAll treeData={this.globalTreeData} />
       </div>
     );
+  }
+
+  createObj = (key, title) => ({
+    key, title, children: []
+  })
+
+  rc = (array, title, newKey) => {
+    if (!array[0]) {
+      if (!newKey) newKey = "0-0";
+      const temp = this.createObj(newKey, title); //Object.assign({},new MyNode(newKey,title))
+      array[0] = temp;
+      console.log(temp)
+    } else {
+      const temp = array[0];
+      const newKey = temp.key + "-0";
+      this.rc(temp.children, title, newKey);
+    }
   }
 }
 
